f2279d88cd5b6d430cdb918bd289daa7b8951b75
Jarrod Medrano <jarrodmedrano@Jarrods-MacBook-Pro.local>
2018-03-10 16:35:29 -0700

    initial commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer>
+    <nav class="navbar navbar-expand-md navbar-light bg-light">
+      <ul class="navbar-nav">
+        <li class="nav-item"><a class="nav-link" href="">home</a></li>
+        <li class="nav-item"><a class="nav-link" href="">menu</a></li>
+        <li class="nav-item"><a class="nav-link" href="">admin</a></li>
+      </ul>
+    </nav>
+  </footer>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+    <nav class="navbar navbar-expand-md navbar-light bg-light">
+      <a class="navbar-brand" href="/">PIZZA PLANET</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+          <li class="nav-item"><a class="nav-link" href="">home</a></li>
+          <li class="nav-item"><a class="nav-link" href="">menu</a></li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<style>
+  header {
+    margin-bottom: 20px;
+  }
+
+  .navbar-brand {
+    font-size: 1.5em;
+  }
+</style>
