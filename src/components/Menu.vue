d664119d74a62fc5f58d26e611d127c76974feeb
Jarrod Medrano <jarrodmedrano@Jarrods-MacBook-Pro.local>
2018-03-10 18:41:43 -0700

    adding shopping basket

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -16,10 +16,29 @@
         <tr v-for="option in item.options">
           <td>{{option.size}}</td>
           <td>{{option.price}}</td>
-          <td><button class="btn btn-sm btn-outline-success" type="button">+</button></td>
+          <td><button class="btn btn-sm btn-outline-success" type="button" @click="addToBasket(item, option)">+</button></td>
         </tr>
         </tbody>
       </table>
+    </div>
+    <div class="col-sm-12 col-md-6">
+      <table class="table">
+        <thead class="thead-default">
+        <tr>
+          <th>Quantity</th>
+          <th>Item</th>
+          <th>Total</th>
+        </tr>
+        </thead>
+        <tbody>
+          <td><button class="btn btn-sm" type="button">-</button><span>1-</span>
+          <button class="btn btn-sm" type="button">+</button></td>
+          <td>Margherita 9"</td>
+          <td>9.95</td>
+        </tbody>
+      </table>
+      <p>Order total: </p>
+      <button class="btn btn-success black">Place order</button>
     </div>
   </div>
 </template>
@@ -28,6 +47,7 @@
   export default {
     data() {
       return {
+        basket: [],
         getMenuItems: {
           1: {
             'name': 'Margherita',
@@ -64,6 +84,17 @@
           }
         }
       }
+    },
+  methods: {
+    addToBasket(item, option) {
+      console.log('press');
+      this.basket.push({
+        name: item.name,
+        price: option.price,
+        size: option.size,
+        quantity: 1
+      })
     }
+  }
   }
 </script>
