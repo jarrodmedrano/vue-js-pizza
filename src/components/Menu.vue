e7e6a6313b691e041c79b9d20a834636fe84ecb1
Jarrod Medrano <jarrodmedrano@Jarrods-MacBook-Pro.local>
2018-03-10 18:07:54 -0700

    adding data to menu

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -9,13 +9,13 @@
           <th>Add to basket</th>
         </tr>
         </thead>
-        <tbody>
+        <tbody v-for="item in getMenuItems">
         <tr>
-          <td><strong>Margherita</strong></td>
+          <td><strong>{{item.name}}</strong></td>
         </tr>
-        <tr>
-          <td>9"</td>
-          <td>12.95</td>
+        <tr v-for="option in item.options">
+          <td>{{option.size}}</td>
+          <td>{{option.price}}</td>
           <td><button class="btn btn-sm btn-outline-success" type="button">+</button></td>
         </tr>
         </tbody>
@@ -23,3 +23,47 @@
     </div>
   </div>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        getMenuItems: {
+          1: {
+            'name': 'Margherita',
+            'description': 'A delicious tomato based pizza topped with mozzarella',
+            'options': [{
+              'size': 9,
+              'price': 6.95
+            }, {
+              'size': 12,
+              'price': 10.95
+            }]
+          },
+          2: {
+            'name': 'Pepperoni',
+            'description': 'A delicious tomato based pizza topped with mozzarella and pepperoni',
+            'options': [{
+              'size': 9,
+              'price': 7.95
+            }, {
+              'size': 12,
+              'price': 12.95
+            }]
+          },
+          3: {
+            'name': 'Ham and Pineapple',
+            'description': 'A delicious tomato based pizza topped with mozzarella, ham and pineapple',
+            'options': [{
+              'size': 9,
+              'price': 7.95
+            }, {
+              'size': 12,
+              'price': 12.95
+            }]
+          }
+        }
+      }
+    }
+  }
+</script>
