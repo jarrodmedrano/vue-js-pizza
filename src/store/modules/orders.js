b76ff6f3522c73d2255ed1dcccda76f7c7f74c54
Jarrod Medrano <jarrodmedrano@Jarrods-MacBook-Pro.local>
2018-03-17 18:53:08 -0600

    reorganizing

---
+++ src/store/modules/orders.js
@@ -0,0 +1,20 @@
+const state = {
+  orders: []
+};
+
+const mutations = {
+  addOrder: (state, orders) => state.orders.push(orders)
+};
+
+const getters = {
+  numberOfOrders: state => state.orders.length
+};
+
+const actions = {};
+
+export default {
+  state,
+  mutations,
+  getters,
+  actions
+};
